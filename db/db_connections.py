e8c7ab4621149b8e9980c47b89da4feb06f95bf5
netteyr14 <753951852456arvin@gmail.com>
2025-09-17 00:42:56 +0800

    [UPDATE] view_class_sched update

--- db/db_connections.py
+++ db/db_connections.py
@@ -4,7 +4,7 @@ db0 = {
     'host': 'localhost',                             # ngrok/aiven host
     'user': 'root',                                  # root/mysql_acc/aiven user
     'password': 'Ambin123456123456',                 # database password for root/mysql_acc/aiven
-    'database': 'dbipt',                             # database name for root/mysql_acc/aiven
+    'database': 'dbcstakiosk',                             # database name for root/mysql_acc/aiven
     'port': 3306                                     # default mysql/aiven port
     #'ssl_ca': os.environ.get('AIVEN_CA_CERT')       # path to aiven ca certificate
 }
