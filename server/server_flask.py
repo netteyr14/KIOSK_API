2f91ba066a1a920fb270ef45b9d794f06ae71f4d
netteyr14 <753951852456arvin@gmail.com>
2025-09-19 21:01:46 +0800

    [UPDATE] Structure Changes

--- server/server_flask.py
+++ server/server_flask.py
@@ -1,12 +1,13 @@
-from flask import Flask, jsonify, request
-from get_functions.load_tables import get_tbl_class_sched, get_tbl_student_management
-from get_functions.load_cbo import get_course_name, get_section, get_year
+from flask import Flask, request
+from routes_function.get_request import get_tbl_class_sched, get_tbl_student_management, get_course_name, get_section, get_year
+from routes_function.post_request import post_student_management
+
 app = Flask(__name__)
 
-#Routes
+#Load/Get Routes
 @app.route('/view_class_sched', methods=['GET'])
 def load_tbl_class_sched():
-    student_number = request.args.get("student_number")  # key matches C# dictionary
+    student_number = request.args.get("stud_number")  # key matches C# dictionary
     return get_tbl_class_sched(student_number)
 
 @app.route('/view_student_management', methods=['GET'])
@@ -24,6 +25,13 @@ def load_tbl_section():
 @app.route('/tbl_year', methods=['GET'])
 def load_tbl_year():
     return get_year()
+
+#Post Routes
+@app.route('/student_information', methods=['POST'])
+def insert_student_information():
+    student_info = request.get_json()
+    return post_student_management(student_info)
+
 #Main
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
