69f97b7dca4fbdeed1aeb0daf319ed07dd026ca3
netteyr14 <753951852456arvin@gmail.com>
2025-09-20 00:08:24 +0800

    [UPDATE] Batch file

--- start_server.bat
+++ start_server.bat
@@ -6,5 +6,5 @@ REM Start Flask app on port 5002
 start cmd /k "waitress-serve --listen=localhost:5002 server.server_flask:app"
 
 REM Start Nginx
-cd /d D:\KIOSK_API\server\nginx-1.28.0
+cd server\nginx-1.28.0
 start cmd /k "nginx.exe"
