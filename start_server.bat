50215b2b1d718080e7692f400ca698e76f6b3d48
netteyr14 <753951852456arvin@gmail.com>
2025-09-20 15:48:19 +0800

    [UPDATE] start_server.bat

--- start_server.bat
+++ start_server.bat
@@ -1,8 +1,8 @@
 @echo on
-REM Start Flask app on port 5001
+REM Start Waitress app on port 5001
 start cmd /k "python -m server.run_waitress --port=5001"
 
-REM Start Flask app on port 5002
+REM Start Waitress app on port 5002
 start cmd /k "python -m server.run_waitress --port=5002"
 
 REM Start Nginx
