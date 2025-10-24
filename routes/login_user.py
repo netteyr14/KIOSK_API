47d5b366d606a54f050a42682182a5e17fc22997
netteyr14 <753951852456arvin@gmail.com>
2025-10-24 09:51:43 +0800

    [UPDATE] /login/user_credential

--- routes/login_user.py
+++ routes/login_user.py
@@ -1,10 +1,10 @@
 from flask import Blueprint, request
 from controllers.login_controller import login_user_function
 
-login_bp = Blueprint('login_bp', __name__)
+login_bp = Blueprint('login_bp', __name__, url_prefix='/login') #adds this first before routes
 
 #LOGIN
-@login_bp.route('/login_user', methods=['POST'])
+@login_bp.route('/user_credential', methods=['POST'])
 def login_user():
     credentials = request.get_json()
     return login_user_function(credentials)
